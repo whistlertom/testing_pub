41f9a5e8e5d36f2de8bf0c30c6b92895ceb3cfd3
whistlertom <tom1506@hotmail.com>
2023-05-23 12:38:15 +0100

    Adding a random python programme to simulate an existing repo

---
+++ Testing.py
@@ -0,0 +1,24 @@
+# A random python programme to add to the repo
+import random
+
+def select_random_emails(filename, number):
+    
+    k =int(number)
+
+    with open(filename) as file_object:
+        lines = file_object.readlines()
+
+    email_list =[]
+    for line in lines:
+        email_list.append(line)
+
+  
+    results = random.sample(email_list, k=k)
+    results.sort()
+    
+    for result in results:
+        print(result)
+
+filename = input("Firstly enter the name of the file containing the list of emails: ")
+number = input("Now enter how many emails you want selected: ")
+select_random_emails(filename, number)
