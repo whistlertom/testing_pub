b59e419d6947bf91540563b81ba1bb674d4e83cd
whistlertom <tom1506@hotmail.com>
2023-05-23 12:45:46 +0100

    Adding some random test secrets to see if I can trigger push protection

--- Testing.py
+++ Testing.py
@@ -22,3 +22,9 @@ def select_random_emails(filename, number):
 filename = input("Firstly enter the name of the file containing the list of emails: ")
 number = input("Now enter how many emails you want selected: ")
 select_random_emails(filename, number)
+
+StripeKeyPub = 'sk_test_26PHem9AhJZvU623DfE1x4sd'
+StripeKeyPriv = 'pk_test_qblFNYngBkEdjEZ16jxxoWSM'
+
+Testsecret = 'dfnsdpijp354tsildf'
+User = 'test@usernet.com'
